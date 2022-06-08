331e5e76cfef2d40a67f99f44d77a7b0fb826350
Rood-Landy Thezard <thezardrood@gmail.com>
2022-06-08 13:17:23 -0400

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Rood-Landy Thezard on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
